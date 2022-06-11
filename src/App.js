a4f1cb318c385e0b967d9efb49e257cff048104c
Leo Mart <mistikleo7@gmail.com>
2022-06-11 15:12:23 +0300

    finally fixed bug with two setStates overriding each other

--- src/App.js
+++ src/App.js
@@ -8,26 +8,27 @@ const PLAYER = {
   money: 10,
   prod: 0,
   mult: 1,
+  generators: [{
+    name: "Gen I", prod: 0
+  }, {
+    name: "Gen II", prod: 0
+  }, {
+    name: "Gen III", prod: 0
+  }],
 }
-
-const PLAYER_GENERATORS = [{
-  name: "Gen I", prod: 0
-}, {
-  name: "Gen II", prod: 0
-}, {
-  name: "Gen III", prod: 0
-}];
 
 
 const App = () => {
 
   const [player, setPlayerData] = useState(PLAYER);
-  const [playerGenerators, setPlayerGenerators] = useState(PLAYER_GENERATORS);
 
   useEffect(() => {
+
     const intervalId = setInterval(() => {
-      setPlayerData({ ...player, money: (player.money + player.prod / 20) });
-    }, 50)
+      setPlayerData(prevState => {
+        return { ...prevState, money: (player.money + player.prod / 20) }
+      });
+    }, 50);
 
     return () => {
       clearInterval(intervalId);
@@ -36,7 +37,7 @@ const App = () => {
 
   useEffect(() => {
     setPlayerData({ ...player, prod: calcProd() });
-  }, [playerGenerators]);
+  }, [player.generators]);
 
 
   const canBuyGenerator = (generatorCost) => {
@@ -44,22 +45,16 @@ const App = () => {
     else return true;
   }
 
-
-
-  const calcProd = () => playerGenerators.reduce((totalProduction, element) => totalProduction + element.prod, 0);
+  const calcProd = () => player.generators.reduce((totalProduction, element) => totalProduction + element.prod, 0);
 
   const updateGameData = (generator) => {
-
     const previousGeneratorCost = generator.cost / 1.2;
-
-    setPlayerData({ ...player, money: player.money - previousGeneratorCost, });
-    setPlayerGenerators(recalculateGenerators(generator));
+    setPlayerData({ ...player, money: player.money - previousGeneratorCost, generators: recalculateGenerators(generator) });
   }
 
   const recalculateGenerators = (generator) => {
-    const newGenerators = playerGenerators.map((el) => 
-    ({...el, prod: (el.name === generator.name) ? generator.prod : el.prod }));
-
+    const newGenerators = player.generators.map((el) =>
+      ({ ...el, prod: (el.name === generator.name) ? generator.prod : el.prod }));
     return newGenerators;
   }
 
